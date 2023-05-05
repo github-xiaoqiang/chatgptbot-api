493e4ef73edbec45270f45a4a72824bcd6868c54
xiaoqiang <1206489852@qq.com>
2023-05-05 16:32:53 +0800

    init ddd struct

---
+++ chatbot-api-interfaces/src/test/java/test/ApiTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+/**
+ * @author 小傅哥，微信：fustack
+ * @description 单元测试
+ * @github https://github.com/fuzhengwei
+ * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
+public class ApiTest {
+
+
+
+}
---
+++ chatbot-api-interfaces/src/test/java/test/SpringBootRunTest.java
@@ -0,0 +1,28 @@
+package test;
+
+
+import com.alibaba.fastjson.JSON;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author 小傅哥，微信：fustack
+ * @description
+ * @github https://github.com/fuzhengwei
+ * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRunTest {
+
+
+}
